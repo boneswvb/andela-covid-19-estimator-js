df399b48c62df160a707d0ceebc3c16d35c57d2c
Wim von Benecke <boneswvb@gmail.com>
2020-04-13 22:40:29 +0200

    checking progress

--- src/estimator.js
+++ src/estimator.js
@@ -21,7 +21,6 @@ const covid19ImpactEstimator = (data) => {
     let elapseTime = 0;
     if (periodType === 'days') {
       elapseTime = timeToElapse;
-      console.log('days', elapseTime)
     } else if (periodType === 'weeks') {
       elapseTime = timeToElapse * 7;
     } else if (periodType === 'months') {
@@ -34,27 +33,4 @@ const covid19ImpactEstimator = (data) => {
 
 covid19ImpactEstimator(mydata.adata);
 
-// const input = data;
-// const impact = {};
-// const severeImpact = {};
-// export default covid19ImpactEstimator;
-  // const calculateImpactCurrentlyInfected = (idata) => {
-  //   const calCurrentlyInfected = idata.adata.reportedCases * 10 * calculateTimeToElapse(data);
-  //   return (calCurrentlyInfected);
-  // };
-  // const calculateSevereImpactCurrentlyInfected = (sdata) => {
-  //   const calSerCurrentlyInfected = sdata.adata.reportedCases * 50 * calculateTimeToElapse(data);
-  //   return (calSerCurrentlyInfected);
-  // };
-  // const calculateImpactinfectionsByRequestedTime = (idata) => {
-  //   const calInfectionsByRequestedTime = idata.impact
-  //     .currentlyInfected * 2 ** Math.floor(calculateTimeToElapse(data) / 3);
-  //   return (calInfectionsByRequestedTime);
-  // };
-  // const calculateSevereImpactinfectionsByRequestedTime = (sdata) => {
-  //   const calSerInfectionsByRequestedTime = sdata.severeImpact
-  //     .currentlyInfected * 2 ** Math.floor(calculateTimeToElapse(data) / 3);
-  //   return (calSerInfectionsByRequestedTime);
-  // };
-// console.log('impact', impact);
-// console.log('severeImpact', severeImpact);
+export default covid19ImpactEstimator;
