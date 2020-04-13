b7c63614e69d16532068db44da7dce4489016408
Wim von Benecke <boneswvb@gmail.com>
2020-04-13 22:34:22 +0200

    checking progress

--- src/estimator.js
+++ src/estimator.js
@@ -11,78 +11,50 @@ const mydata = {
     reportedCases: 674,
     population: 66622705,
     totalHospitalBeds: 1380614
-  },
-  data: {
-    periodType: ''
-  },
-  impact: {
-    currentlyInfected: 0,
-    infectionsByRequestedTime: 0,
-    severeCasesByRequestedTime: 0,
-    hospitalBedsByRequestedTime: 0,
-    casesForICUByRequestedTime: 0,
-    casesForVentilatorsByRequestedTime: 0,
-    dollarsInFlight: 0
-  },
-  severeImpact: {
-    currentlyInfected: 1,
-    infectionsByRequestedTime: 0,
-    severeCasesByRequestedTime: 0,
-    hospitalBedsByRequestedTime: 0,
-    casesForICUByRequestedTime: 0,
-    casesForVentilatorsByRequestedTime: 0,
-    dollarsInFlight: 0
   }
 };
 
 const covid19ImpactEstimator = (data) => {
-  // const calculateTimeToElapse = (dwmdata) => {
-  //   let elapseTime = 0;
-  //   if (data.adata.periodType === 'days') {
-  //     elapseTime = dwmdata.adata.timeToElapse;
-  //   } else if (data.adata.periodType === 'weeks') {
-  //     elapseTime = dwmdata.adata.timeToElapse * 7;
-  //   } else if (data.adata.periodType === 'months') {
-  //     elapseTime = dwmdata.adata.timeToElapse * 30;
-  //   }
-  //   return elapseTime;
-  // };
-  // calculateTimeToElapse(data);
+  const { periodType, timeToElapse } = data;
+  
+  const calculateTimeToElapse = () => {
+    let elapseTime = 0;
+    if (periodType === 'days') {
+      elapseTime = timeToElapse;
+      console.log('days', elapseTime)
+    } else if (periodType === 'weeks') {
+      elapseTime = timeToElapse * 7;
+    } else if (periodType === 'months') {
+      elapseTime = timeToElapse * 30;
+    }
+    return elapseTime;
+  };
+  calculateTimeToElapse(periodType, timeToElapse);
+}
 
+covid19ImpactEstimator(mydata.adata);
+
+// const input = data;
+// const impact = {};
+// const severeImpact = {};
+// export default covid19ImpactEstimator;
   // const calculateImpactCurrentlyInfected = (idata) => {
   //   const calCurrentlyInfected = idata.adata.reportedCases * 10 * calculateTimeToElapse(data);
   //   return (calCurrentlyInfected);
   // };
-
   // const calculateSevereImpactCurrentlyInfected = (sdata) => {
   //   const calSerCurrentlyInfected = sdata.adata.reportedCases * 50 * calculateTimeToElapse(data);
   //   return (calSerCurrentlyInfected);
   // };
-
   // const calculateImpactinfectionsByRequestedTime = (idata) => {
   //   const calInfectionsByRequestedTime = idata.impact
   //     .currentlyInfected * 2 ** Math.floor(calculateTimeToElapse(data) / 3);
   //   return (calInfectionsByRequestedTime);
   // };
-
-
   // const calculateSevereImpactinfectionsByRequestedTime = (sdata) => {
   //   const calSerInfectionsByRequestedTime = sdata.severeImpact
   //     .currentlyInfected * 2 ** Math.floor(calculateTimeToElapse(data) / 3);
   //   return (calSerInfectionsByRequestedTime);
   // };
-
-  const assignPeriodType = (ddata) => {
-    const dwmperiodType = ddata.adata.periodType;
-    return (dwmperiodType);
-  };
-  // mydata.impact.currentlyInfected = calculateImpactCurrentlyInfected(data);
-  // mydata.severeImpact.currentlyInfected = calculateSevereImpactCurrentlyInfected(data);
-  // mydata.impact.infectionsByRequestedTime = calculateImpactinfectionsByRequestedTime(data);
-  // mydata.severeImpact
-  //   .infectionsByRequestedTime = calculateSevereImpactinfectionsByRequestedTime(data);
-  mydata.data.periodType = assignPeriodType(data);
-};
-// covid19ImpactEstimator(mydata);
-//  console.log("mydata", mydata)
-export default covid19ImpactEstimator;
+// console.log('impact', impact);
+// console.log('severeImpact', severeImpact);
