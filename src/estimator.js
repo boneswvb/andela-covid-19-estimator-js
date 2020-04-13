3eb57d39a2e0298ddb417c9ee7cfa7af8bfbbde9
Wim von Benecke <boneswvb@gmail.com>
2020-04-13 23:37:54 +0200

    checking progress

--- src/estimator.js
+++ src/estimator.js
@@ -1,34 +1,32 @@
-const mydata = {
-  adata: {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 28,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  }
-};
-
+// const mydata = {
+//   adata: {
+//     region: {
+//       name: 'Africa',
+//       avgAge: 19.7,
+//       avgDailyIncomeInUSD: 5,
+//       avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: 'days',
+//     timeToElapse: 28,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+//   }
+// };
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  let impact = {};
+  const impact = {};
   const severeImpact = {};
   const { periodType, timeToElapse } = data;
   const calculateTimeToElapse = () => {
     let elapseTime = 0;
-    let result = 0;
     if (periodType === 'days') {
       elapseTime = timeToElapse;
     } else if (periodType === 'weeks') {
       elapseTime = timeToElapse * 7;
     } else if (periodType === 'months') {
       elapseTime = timeToElapse * 30;
-    };
+    }
     return (elapseTime);
   };
   calculateTimeToElapse();
