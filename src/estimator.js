fbef00ebfc945df02f34db2061cc6fe629c2e2c3
Wim von Benecke <boneswvb@gmail.com>
2020-04-14 04:49:24 +0200

    checking progress

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,28 @@
-// const mydata = {
-//   adata: {
-//     region: {
-//       name: 'Africa',
-//       avgAge: 19.7,
-//       avgDailyIncomeInUSD: 5,
-//       avgDailyIncomePopulation: 0.71
-//     },
-//     periodType: 'days',
-//     timeToElapse: 10,
-//     reportedCases: 674,
-//     population: 66622705,
-//     totalHospitalBeds: 1380614
-//   }
-// };
+const mydata = {
+  adata: {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 10,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  }
+};
 const covid19ImpactEstimator = (data) => {
   // const input = data;
   const impact = {};
   const severeImpact = {};
-  const { periodType, timeToElapse, totalHospitalBeds, reportedCases } = data;
+  const { 
+    periodType, 
+    timeToElapse, 
+    totalHospitalBeds, 
+    reportedCases 
+  } = data;
   const calculateTimeToElapse = () => {
     let elapseTime = 0;
     if (periodType === 'days') {
@@ -40,19 +45,18 @@ const covid19ImpactEstimator = (data) => {
     .infectionsByRequestedTime);
   severeImpact.severeCasesByRequestedTime = Math.floor(0.15 * severeImpact
     .infectionsByRequestedTime);
-  impact.hospitalBedsByRequestedTime = Math.floor((35 / 100) * totalHospitalBeds) - impact
+  impact.hospitalBedsByRequestedTime = Math.trunk((35 / 100) * totalHospitalBeds) - impact
     .severeCasesByRequestedTime;
   severeImpact
-    .hospitalBedsByRequestedTime = Math.floor((35 / 100) * totalHospitalBeds) - severeImpact
+    .hospitalBedsByRequestedTime = Math.trunk((35 / 100) * totalHospitalBeds) - severeImpact
       .severeCasesByRequestedTime;
-  // const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data;
-  // console.log('impact', impact)
-  // console.log('severeImpact', severeImpact)
+  console.log('impact', impact)
+  console.log('severeImpact', severeImpact)
   return {
     data,
     impact,
     severeImpact
   };
 };
-// covid19ImpactEstimator(mydata.adata);
-export default covid19ImpactEstimator;
+covid19ImpactEstimator(mydata.adata);
+// export default covid19ImpactEstimator;
