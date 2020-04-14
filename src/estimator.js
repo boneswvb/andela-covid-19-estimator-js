146042cb686c7882cfcae84f43b22d9ba9c28cad
Wim von Benecke <boneswvb@gmail.com>
2020-04-14 06:22:13 +0200

    checking progress

--- src/estimator.js
+++ src/estimator.js
@@ -69,9 +69,9 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.casesForVentilatorsByRequestedTime = Math.floor(0.02 * severeImpact
     .infectionsByRequestedTime);
   impact.dollarsInFlight = Math
-    .floor(((population * avgDailyIncomePopulation) * avgDailyIncomeInUSD) * 30);
+    .floor(((population * avgDailyIncomePopulation) * avgDailyIncomeInUSD) / 30);
   severeImpact.dollarsInFlight = Math
-    .floor(((population * avgDailyIncomePopulation) * avgDailyIncomeInUSD) * 30);
+    .floor(((population * avgDailyIncomePopulation) * avgDailyIncomeInUSD) / 30);
   // console.log('impact', impact)
   // console.log('severeImpact', severeImpact)
   return {
